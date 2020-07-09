d9d008eee4c489c2ddbcd4c7b81af6140b1233a2
lucas <lucasvicente2@hotmail.com>
2020-07-09 00:30:31 -0300

    09-07-2020 00:28 - edit readme and adjustment in the exception flow

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# webscraping_python
-Web Scraping - application for study
+# WEB SCREPING IN PYTHON
+**This application is for educational purposes.**
+
+The application has the objective of searching for a movie and returning all the data related to the search.
+
+Scraping done on the website to search for films and their related information.
+
+Web Scraping In:
+- [https://pastebin.com/PcVfQ1ff](https://pastebin.com/PcVfQ1ff)
+- [https://pastebin.com/Tdp532rr](https://pastebin.com/Tdp532rr)
+
+## Getting Started Guide
+This guide is intended to instruct the installation on your machine
+
+### Prerequisites
+- Python 3.x
+### Requirements
+  
+The requirements are for installing libs for the application
+
+Execute CLI:
+```sh 
+    pip install -r requeriments.txt
+```
+## Using Application
+
+### Run code:
+```sh 
+    python main.py
+```
+### Application running:
+
+
+    ###### Informação de Filmes ######
+    
+```sh   
+    Entre com Genero/Filme: ex.: terror/a vila   
+```
+  
+### Insert data
+Inserting data into the machine console
+```sh
+
+    terror/a vila
+```
+### Response
+```sh
+
+    {
+        "url": "https://pastebin.com/26ApMRdm",
+        "titulo": "a vila",
+        "genero": "terror",
+        "diretor": "reginaldo catarino",
+        "duracao": "02:12"
+    }
+
+```
--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def get_html(url):
 
         return table
 
-    except:
+    except Exception:
 
-        print("Incorreta a leitura do HTML")
+        print("Incorrect HTML Reading")
 
 def get_html_link(url):
 
@@ -34,64 +34,82 @@ def get_html_link(url):
 
     soup = BeautifulSoup(link, 'html.parser')
 
-    html_parse = soup.find("textarea", class_="paste_code")
-    inside_textarea = html_parse.next_element
+    try:
 
-    soup_table = BeautifulSoup(inside_textarea, 'html.parser')
-    table = soup_table.find_all( 'dd' )
-    PAGE_LINK = []
+        html_parse = soup.find("textarea", class_="paste_code")
+        inside_textarea = html_parse.next_element
 
-    for tag in table:
-        PAGE_LINK.append(tag.string)
+        soup_table = BeautifulSoup(inside_textarea, 'html.parser')
+        table = soup_table.find_all( 'dd' )
+        PAGE_LINK = []
 
-    PAGE_LINK.append(url) 
-    return (PAGE_LINK)
-    
+        for tag in table:
+            PAGE_LINK.append(tag.string)
+
+        PAGE_LINK.append(url) 
+        return (PAGE_LINK)
+
+    except Exception:
+
+        print("Incorrect HTML Reading")
+        
 
 def get_movie_info(listing_url, links_url, movie_id):
 
- 
-        split_movie_id = movie_id.split("/")
+    
+    split_movie_id = movie_id.split("/")
+    number_split_movie_id = len(split_movie_id)
 
-        genre = split_movie_id[0]
-        name = split_movie_id[1]
+    if ( number_split_movie_id < 2 ):
+        return print("Incorrect syntax")
+
+    genre = split_movie_id[0]
+    name = split_movie_id[1]
+
+    table = get_html(listing_url)
+    link = get_html(links_url)
+
+    try:
         
-        table = get_html(listing_url)
-        link = get_html(links_url)
-
         df_full_filmes = pd.read_html( str(table))[0]
         df_full_filmes_link = pd.read_html( str(link))[0]
 
         df_filter_movie = df_full_filmes.loc[(df_full_filmes['nome'] == name) & 
             (df_full_filmes['genero'] == genre) ]
 
-        diretor_filme = df_filter_movie['diretor'].unique()[0]
-        genero_filme = df_filter_movie['genero'].unique()[0]
-         
+        if (df_filter_movie.empty):
+            return print("Movie not found")
+            
+        director_movie = df_filter_movie['diretor'].unique()[0]
+        genre_movie = df_filter_movie['genero'].unique()[0]
+            
         df_links = df_full_filmes_link.loc[(df_full_filmes_link['nome'] == name) ]
 
         df_li = df_links.get(key='link')
 
-        PAGE_LINK = []
-         
+        PAGE_LINK = []            
 
         for number_page_link in df_li:
             PAGE_LINK.append(get_html_link(number_page_link))
-         
+            
         for number_movie in PAGE_LINK:
-            if name == number_movie[0] and diretor_filme == number_movie[2]:
+            if name == number_movie[0] and director_movie == number_movie[2]:
                 response_movie = (number_movie)
 
         response = {
             "url": response_movie[3],
             "titulo": response_movie[0],
-            "genero": genero_filme,
+            "genero": genre_movie,
             "diretor": response_movie[2],
             "duracao": response_movie[1]
         }
 
         return print( json.dumps(response,indent=4) )
- 
+
+    except Exception:
+
+        print("Error")
+
  
 get_movie_info(
     "https://pastebin.com/PcVfQ1ff",
